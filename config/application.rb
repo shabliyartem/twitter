117320ce8d28c22ea8a6aff35efb4b5d265fc7fc
Artem Shabliy <artem.shabliy@rezonans.ru>
2012-08-23 11:18:38 +0300

    assets precompile

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-#config.assets.initialize_on_precompile = false
+config.assets.initialize_on_precompile = false
 
 module Twitter
   class Application < Rails::Application
